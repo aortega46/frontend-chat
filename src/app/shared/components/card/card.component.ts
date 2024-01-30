cb3e63b2a93b0e43ba9c44a86e05ad3f51b0b602
Albert Ortega <trebla.og46@gmail.com>
2024-01-30 15:58:17 +0100

    Default avatar when no image

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core'
+import { NgClass, NgStyle } from '@angular/common'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core'
 
+import { colors } from '../../utils/cardColors'
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
-  // TODO: Input name and image
+export class CardComponent implements OnInit, AfterViewInit {
+  // TODO: Input image
+  @ViewChild('avatarInitials') avatar?: ElementRef
+
+  @Input() name: string = 'RANDOM'
+  @Input() img?: string
+
+  private initials?: string
+
+  ngOnInit(): void {
+    this.getInitials()
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.img) {
+      this.avatar!.nativeElement.innerText = this.initials
+    }
+  }
+
+  getInitials() {
+    this.initials = this.name
+      .split(' ')
+      .slice(0, 2)
+      .map(w => w.charAt(0).toUpperCase())
+      .join('')
+  }
+
+  get getColor(): string {
+    const charIndex = (this.initials!.charCodeAt(0) - 65) % colors.length
+    const customColor = colors[charIndex]
+    return customColor
+  }
 }
